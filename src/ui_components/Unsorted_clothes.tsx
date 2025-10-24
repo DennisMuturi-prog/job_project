df509194c28ecb0fa72382886f5a7970d85d48e6
Dennis Muturi <deynohmuturi@gmail.com>
2025-10-24 22:33:32 +0300

    style: Improve code formatting and spacing in Unsorted_clothes component

--- src/ui_components/Unsorted_clothes.tsx
+++ src/ui_components/Unsorted_clothes.tsx
@@ -2,16 +2,18 @@ import type { Item } from '@/types'
 import React from 'react'
 import Draggable from './Draggable'
 
+
 interface UnsortedProps {
-    items:Item[]
+  items: Item[]
 }
 
-function Unsorted_clothes({items}:UnsortedProps) {
+function Unsorted_clothes({ items }: UnsortedProps) {
   return (
-    <div className='flex gap-3 justify-center'>
-        {items.map((item) => (
+    <div className='flex gap-8 justify-center'>
+      {items.map((item) => (
         <Draggable key={item.id} id={item.id} >
-            <img src={item.asset_src} alt="" width={80} height={80}/>
+          <img src={item.asset_src} alt="" width={80} height={80}/>
+          
         </Draggable>
       ))}
 
